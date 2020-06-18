5e27124d4b00aa5f9e486852b13c038af9d4c1b5
Albert Nah <albert.nah@groundx.xyz>
2020-06-18 09:18:54 +0900

    init

--- App.tsx
+++ App.tsx
@@ -1,47 +1,87 @@
 import './global';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  View,
+  Image,
+  TouchableHighlight,
+  Alert
+} from 'react-native';
 const Caver = require('caver-js');
+const eoa = "0xD85FCeCE453d605afcbf6924C3Bc803060161C36";
 
 interface States {
   latestBlock: number
+  balance: number
 }
 
 export default class App extends React.Component<{}, States> {
-  constructor() {
-    super({});
+  constructor(props: Readonly<{}>) {
+    super(props);
     this.state = {
       latestBlock: -1,
+      balance: 9.999,
     }
   }
 
-  componentWillMount() {
+  componentDidMount() {
     const caver = new Caver('https://api.baobab.klaytn.net:8651/');
-  
-    caver.klay.getBlock('latest').then((latestBlock: any) => {
-      console.log(latestBlock)
-      this.setState({latestBlock: caver.utils.hexToNumber(latestBlock.number)})
-    })
+
+    let refresh = () => {
+      caver.klay.getBlock('latest').then((latestBlock: any) => {
+        this.setState({ latestBlock: caver.utils.hexToNumber(latestBlock.number) })
+      })
+      caver.klay.getBalance(eoa).then((balance: any) => {
+        this.setState({ balance: caver.utils.fromPeb(balance) })
+      })
+    }
+    refresh()
+
+    setInterval(refresh, 5000);
   }
 
   render() {
     const latestBlockNumber = this.state.latestBlock;
+    const balance = this.state.balance;
 
     return (
-      <View style={styles.container}>
-        <Text>Latest klaytn block number is: {latestBlockNumber}</Text>
-        <Text>Check your console!</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <Image style={styles.stretch}
+          source={require('./assets/klip.png')}
+        />
+        <Text></Text>
+        <Text>* EOA: {eoa}</Text>
+        <Text></Text>
+        <Text>* Your KLAY: {balance}</Text>
+        <Text></Text>
+        <Text>* Latest BN: {latestBlockNumber}</Text>
+        <Text></Text>
+
+        <View>
+          <TouchableHighlight style={styles.button} onPress={() => Alert.alert('TBA')}>
+            <Text>Kakao Login</Text>
+          </TouchableHighlight>
+        </View>
+      </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'flex-start',
-    backgroundColor: '#fff',
+    alignItems: 'stretch',
     flex: 1,
-    justifyContent: 'center',
-    margin: 20,
+    margin: 0,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#FFFF00",
+    padding: 20
+  },
+  stretch: {
+    width: 415,
+    height: 400,
   },
 });
